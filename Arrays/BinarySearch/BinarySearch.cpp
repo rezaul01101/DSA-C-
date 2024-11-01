8b0f377bcfe295c9ea103bbee2e2ffcc10a170c7
Rezaul Hoque <rezaul01101@gmaail.com>
2024-11-01 07:41:17 +0600

    [Arrays] Binary search function create

--- Arrays/BinarySearch/BinarySearch.cpp
+++ Arrays/BinarySearch/BinarySearch.cpp
@@ -1,6 +1,40 @@
-#include<iostream>>
+#include <iostream>
 using namespace std;
 
-int main(){
-     
+int binarySearch(int arr[], int size, int key)
+{
+
+    int start = 0;
+    int end = size - 1;
+
+    int mid = (start + end) / 2;
+
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+
+        //goto right part
+        if(key>arr[mid]){
+            start= mid+1;
+        }else{
+            end= mid-1;
+        }
+
+        mid= (start + end) / 2;
+    }
+
+    return -1;
+}
+
+int main()
+{
+
+    int even[6] = {2, 4, 6, 8, 12, 18};
+    int odd[5] = {3, 8, 11, 14, 16};
+    int findKey=4;
+    int evenIndex =binarySearch(even,6,findKey);
+    cout << "Index of "<<findKey <<" is "<<evenIndex <<endl;
 }

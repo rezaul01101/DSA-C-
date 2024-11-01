9b69b5516ce6cc05b983dbda70fa8cda742ea29c
Rezaul Hoque <rezaul01101@gmaail.com>
2024-11-01 07:50:51 +0600

    [Arrays] Binary search mid functionality upgraded

--- Arrays/BinarySearch/BinarySearch.cpp
+++ Arrays/BinarySearch/BinarySearch.cpp
@@ -7,7 +7,8 @@ int binarySearch(int arr[], int size, int key)
     int start = 0;
     int end = size - 1;
 
-    int mid = (start + end) / 2;
+    // int mid = (start + end) / 2;
+    int mid = start +(end-start)/2;
 
     while (start <= end)
     {
@@ -23,7 +24,7 @@ int binarySearch(int arr[], int size, int key)
             end= mid-1;
         }
 
-        mid= (start + end) / 2;
+        int mid = start +(end-start)/2;
     }
 
     return -1;
@@ -34,7 +35,7 @@ int main()
 
     int even[6] = {2, 4, 6, 8, 12, 18};
     int odd[5] = {3, 8, 11, 14, 16};
-    int findKey=4;
+    int findKey=6;
     int evenIndex =binarySearch(even,6,findKey);
     cout << "Index of "<<findKey <<" is "<<evenIndex <<endl;
 }
